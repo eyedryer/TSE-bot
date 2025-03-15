88a9de19459dff84be2d5ad4c0aaf3dd004b9a87
eyedryer <eyedryer0@gmail.com>
2025-03-15 22:45:36 +0200

    Update __init__.py

--- image_bot/__init__.py
+++ image_bot/__init__.py
@@ -1,7 +1,7 @@
-from image_bot.config import sql_name, sql_password, postgres_db
+from image_bot.config import sql_name, sql_password, postgres_db, connection_string
 
 TORTOISE_ORM = {
-    "connections": {"default": f"postgres://{sql_name}:{sql_password}@127.0.0.1:5432/{postgres_db}"},
+    "connections": {"default": f"{connection_string}"},
     "apps": {
         "models": {
             "models": ["image_bot.models", "aerich.models"],
